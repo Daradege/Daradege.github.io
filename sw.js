c05fc92bb1fd9601f4a9d6a093263482c2ae38a7
Ali Safamanesh <136700107+Daradege@users.noreply.github.com>
2025-01-22 12:30:48 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,1 @@
-const cacheName = 'v1';
-const cachedFiles = ['/', 'index.html', '/en', '/blog', '/logo.png', '/tw.js'];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(cacheName).then(cache => {
-            console.log('Opened cache');
-            return cache.addAll(cachedFiles);
-        })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
